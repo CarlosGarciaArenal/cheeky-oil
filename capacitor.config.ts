798deeb220ce81069c17095b5dce7015386825ad
Carlos García Arenal <cga416@alumnos.unican.es>
2026-07-11 18:13:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cheeky-oil',
+  webDir: 'www'
+};
+
+export default config;
